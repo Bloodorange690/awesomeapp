7399d8b88cf0636e02e398bf7764d27ff1673635
Bloodorange690 <bluespruce185@gmail.com>
2020-08-31 15:38:37 +0200

    I added other pokemons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,37 @@ import "./App.css";
 import Title from "./components/Title";
 import Pokemon from "./components/Pokemon";
 
+const pokemons = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: true,
+    abilities: "Solar power, Tough claws, Drought",
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: "Overgrow, Chlorophyll",
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: false,
+    abilities: "Pressure, Unnerve, Steadfast, Insomnia",
+  },
+  {
+    name: "Mega beedrill",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: "Intimidate, Unnerve",
+  },
+];
+
 function App() {
   return (
     <div className="App">
@@ -13,7 +44,7 @@ function App() {
       </main>
 
       <header className="App-header">
-        <p>I want to be an elephant and eat cotton candys all day long</p>
+        <p>I want to be an elephant and eat cotton candies all day long</p>
         <p>I hope it will work</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
