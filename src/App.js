2f092092e6a9fc80b9ac11bd2a6d1408f2f2026e
Bloodorange690 <bluespruce185@gmail.com>
2020-08-31 14:53:50 +0200

    I added pokemon component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Title />
+        <Title content="Some Simple Title" />
+        <Pokemon />
       </main>
 
       <header className="App-header">
